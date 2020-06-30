41bb425474a7062558d24b3eace6481292c3309b
NVasilakop <nvasilakop@gmail.com>
2020-07-01 00:15:28 +0300

    FirstCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { GeomapComponent } from './geomap/geomap.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component:WelcomePageComponent},
+  {path:'geoMap',component:GeomapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
+import { GeomapComponent } from './geomap/geomap.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomepageComponent,
+    GeomapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyDnjISdA_PRe8hdooYyiAL6UJ7iHOsIXT8',
+      libraries:['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
